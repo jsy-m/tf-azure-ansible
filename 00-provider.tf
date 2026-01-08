57520fde464514c3b7674e5015e8cd0a89aa6133
jsy-m <118874092+jsy-m@users.noreply.github.com>
2026-01-08 16:17:50 +0900

    Create 00-provider.tf

---
+++ 00-provider.tf
@@ -0,0 +1,35 @@
+terraform { 
+  cloud {    
+    organization = "rp-sy" 
+    workspaces { 
+      name = "azure-aap" 
+    } 
+  } 
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  
+    aap = {
+      source = "ansible/aap"
+    }
+  }
+}
+
+provider "aap" {
+  host     = "https://10.65.10.9"
+  username = "admin"
+  password = var.aap_password
+  insecure_skip_verify = true
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "e6f54661-ed1d-4d62-9f60-f33454912e34"
+  tenant_id         = "37954b6a-13e5-4c17-8137-5c84015ab986"
+  client_id         = "f0730e98-ea0f-48d6-8437-3aa6da1581e7"
+  client_secret     = var.azure_client_secret
+}
